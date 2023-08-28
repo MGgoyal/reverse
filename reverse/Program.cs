0f636c40ad016828b3d96c4c7e03184a2e2fb8f1
tanvi goyal <tanvi goyal@DESKTOP-Q4I1B7T>
2023-08-28 15:23:50 +0530

    Add project files.

---
+++ reverse/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Sample
+{
+    public static void Main()
+    {
+        int[] intArr = new int[5] { 10, 20, 30, 40, 50 };
+        int loop = 0;
+
+        Array.Reverse(intArr);
+
+        Console.WriteLine("Array after Reverse() method: ");
+        for (loop = 0; loop < intArr.Length; loop++)
+        {
+            Console.WriteLine("Element " + (loop + 1) + " is " + intArr[loop]);
+        }
+    }
+}
